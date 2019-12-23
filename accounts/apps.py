c19e3f53db4f9b63a3e27487c8cb5fa3021b432f
Amir Mullagaliev <amirmullagaliev@gmail.com>
2019-12-23 21:25:13 +0500

    signals improvements

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     name = 'accounts'
     verbose_name = "Аккаунты"
+
+    def ready(self):
+        from accounts.signals import receive_report_signal
--- accounts/signals.py
+++ accounts/signals.py
@@ -5,11 +5,10 @@ from django.dispatch import receiver
 
 from .models import ActivityManager
 from support.models import Report
+from .models import Activity
 
-# activity_manager = ActivityManager()
-#
-#
-# @receiver(post_save, sender=Report)
-# def receive_report_signal(sender, instance, **kwargs):
-#     activity_manager.notify_group(sender=instance.sender, group_name='staff', action="отправил багрепорт",
-#                                   object=instance)
+
+@receiver(post_save, sender=Report)
+def receive_report_signal(sender, instance, **kwargs):
+    Activity.objects.notify_group(sender=instance.sender, group_name='Преподаватели', action="отправил багрепорт",
+                                  object=instance)
