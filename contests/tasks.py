7d3a9066c1082b42617d870697f4efb5b7b9a6b0
Ruslan Bektashev <ruslanbektashev@gmail.com>
2020-03-02 04:27:20 +0300

    set soft time limit for evaluate_submission task

--- contests/tasks.py
+++ contests/tasks.py
@@ -1,24 +1,30 @@
 import mosspy
 
+from billiard.exceptions import SoftTimeLimitExceeded
+
 from django.contrib.auth.models import User
 
 from contest.celery import app
 from contests.observers import TaskProgressObserver
 from contests.models import Submission
+from tools.utility import Status
 
 
-@app.task(bind=True, time_limit=600)
+@app.task(bind=True, soft_time_limit=600, time_limit=660)
 def evaluate_submission(self, submission_id, user_id):
     submission = Submission.objects.get(id=submission_id)
     user = User.objects.get(id=user_id)
 
     observer = TaskProgressObserver(self)
-    submission.evaluate(user, observer)
+    try:
+        submission.evaluate(user, observer)
+    except SoftTimeLimitExceeded:
+        submission.update(Status.TL)
 
     return submission.status, submission.get_status_display()
 
 
-@app.task(bind=True, time_limit=300)
+@app.task(bind=True, time_limit=600)
 def moss_submission(self, submission_id, to_submission_ids):
     submission = Submission.objects.get(id=submission_id)
     to_submissions = Submission.objects.filter(id__in=to_submission_ids)
