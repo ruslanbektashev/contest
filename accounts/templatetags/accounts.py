f28fbe090de52927e1636372a8aac52749dc878c
Evgenia Abdullaeva <evabdullaeva@gmail.com>
2020-11-08 13:08:32 +0500

    Added Account model's unread_comments_count method

--- accounts/templatetags/accounts.py
+++ accounts/templatetags/accounts.py
@@ -13,3 +13,13 @@ def mark_comments_as_read(account_id, obj):
         return
 
     return account.mark_comments_as_read(obj)
+
+
+@register.simple_tag()
+def unread_comments_count(account_id, obj):
+    try:
+        account = Account.objects.get(user_id=account_id)
+    except Account.DoesNotExist:
+        return obj.comment_set.count()
+
+    return account.unread_comments_count(obj)
