ee7f2ade3504718adbe7b52bccc44ac38f068b0b
Evgenia Abdullaeva <evabdullaeva@gmail.com>
2020-11-12 08:54:24 +0500

    creating users without accounts in contests.tests fix

--- accounts/templatetags/accounts.py
+++ accounts/templatetags/accounts.py
@@ -6,20 +6,10 @@ register = template.Library()
 
 
 @register.simple_tag()
-def mark_comments_as_read(account_id, obj):
-    try:
-        account = Account.objects.get(user_id=account_id)
-    except Account.DoesNotExist:
-        return ''
-
+def mark_comments_as_read(account, obj):
     return account.mark_comments_as_read(obj)
 
 
 @register.simple_tag()
-def unread_comments_count(account_id, obj):
-    try:
-        account = Account.objects.get(user_id=account_id)
-    except Account.DoesNotExist:
-        return obj.comment_set.count()
-
+def unread_comments_count(account, obj):
     return account.unread_comments_count(obj)
