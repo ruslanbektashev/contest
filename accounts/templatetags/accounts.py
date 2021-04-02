6812085722b157f383e69d0e167b8c8a28246e2f
Evgenia Abdullaeva <evabdullaeva@gmail.com>
2021-04-02 14:47:25 +0500

    Bugreport, submission, announcement and schedule notifications are marked as read if the user sees the notification object

--- accounts/templatetags/accounts.py
+++ accounts/templatetags/accounts.py
@@ -1,4 +1,6 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
+from accounts.models import Activity
 
 register = template.Library()
 
@@ -6,3 +8,8 @@ register = template.Library()
 @register.simple_tag()
 def unread_comments_count(account, obj):
     return account.unread_comments_count(obj) if hasattr(obj, 'comment_set') else 0
+
+
+@register.simple_tag()
+def mark_notification_as_read(account, obj):
+    Activity.objects.filter(recipient=account.user, object_type=ContentType.objects.get_for_model(obj), object_id=obj.id).mark_as_read()
