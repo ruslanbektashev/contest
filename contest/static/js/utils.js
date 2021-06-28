50257f361af78df4445e542c652dd3687cb6f28a
Ruslan Bektashev <ruslanbektashev@gmail.com>
2021-06-28 13:33:44 +0300

    add confirmation to submission submit button

--- contest/static/js/utils.js
+++ contest/static/js/utils.js
@@ -13,3 +13,21 @@ function toggleHTML(elem, firstHTML, secondHTML) {
         elem.innerHTML = firstHTML;
     }
 }
+
+function toggleOpacity(firstElement, secondElement, firstTime, secondTime) {
+    firstElement.style.transition = 'opacity ' + firstTime;
+    firstElement.style.opacity = '0';
+    const hider = () => {
+        firstElement.hidden = true;
+        firstElement.removeEventListener('transitionend', hider);
+    };
+    const shower = () => {
+        secondElement.hidden = false;
+        const refresh = secondElement.offsetHeight;
+        secondElement.style.transition = 'opacity ' + secondTime;
+        secondElement.style.opacity = '1';
+        firstElement.removeEventListener('transitionend', shower);
+    }
+    firstElement.addEventListener('transitionend', hider)
+    firstElement.addEventListener('transitionend', shower)
+}
