5c98bfbbba92e6778aed9785d1aee16a5ee0c0a0
Ruslan Bektashev <ruslanbektashev@gmail.com>
2021-12-15 12:13:46 +0300

    fix course list template

--- contests/views.py
+++ contests/views.py
@@ -163,7 +163,7 @@ class CourseList(LoginRequiredMixin, ListView):
         return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-        if self.request.user.account.is_instructor and not self.request.user.has_perm('contests.add_faculty'):
+        if not self.request.user.has_perm('contests.add_faculty'):
             course_ids = self.request.user.filter_set.values_list('course', flat=True)
             courses = Course.objects.filter(id__in=course_ids)
         else:
