f1e6a487416d7133d3854f8ea11d28d0f41cabf1
svelval <svelval@mail.ru>
2022-05-23 12:53:33 +0500

    actual announcements showing mechanism now is in 'views'

--- accounts/views.py
+++ accounts/views.py
@@ -533,8 +533,8 @@ class AnnouncementList(LoginRequiredMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
-        context['has_actual_announcements'] = Announcement.objects.get_queryset().filter(
-            actual__gte=timezone.now().today()).exists()
+        context['announcements'] = Announcement.objects.get_queryset().filter(
+            actual__gte=timezone.now().today())
         return context
 
 
