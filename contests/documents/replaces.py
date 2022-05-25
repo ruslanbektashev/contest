c09cd2a06753e980021251d35b55c7b95e952b00
Maxim Pochinskiy <mvp280102@gmail.com>
2022-05-25 21:44:57 +0500

    Rename watermarks.py to replaces.py and add replaces inside.

---
+++ contests/documents/replaces.py
@@ -0,0 +1,19 @@
+# Aspose-Slides watermarks to remove:
+PPT_WM_1 = 'Evaluation only.</tspan>'
+PPT_WM_2 = 'Created with Aspose.Slides for .NET Standard 2.0 22.1.'
+PPT_WM_3 = 'Copyright 2004-2022Aspose Pty Ltd.'
+
+# Styles to replace:
+PPT_ST_1_BEFORE = 'class="slide"'
+PPT_ST_1_AFTER = 'class="slide pagination justify-content-center"  style="margin-bottom: 1%!important;'
+PPT_ST_2_BEFORE = 'xlink="http://www.w3.org/1999/xlink" width='
+PPT_ST_2_AFTER = 'xlink="http://www.w3.org/1999/xlink" class="h-auto d-inline-block" style="width:70%!important"'
+PPT_ST_3_BEFORE = 'class="slideTitle"'
+PPT_ST_3_AFTER = 'style="display:none;"'
+
+# Aspose-Words watermark to remove:
+DOC_WM_1 = 'Evaluation Only. Created with Aspose.Words. Copyright 2003-2022 Aspose Pty Ltd.'
+
+# Others:
+BLANK = ' '
+TSPAN = ' </tspan>'
