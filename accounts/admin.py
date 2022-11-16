a62d680b58a0e5ded3637ec44352bf14c2e3036b
Ruslan Bektashev <ruslanbektashev@gmail.com>
2022-11-16 17:31:09 +0300

    show LogEntry in admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 from django.contrib.auth.models import Permission
 
 from accounts.models import Account, Comment, Faculty, Announcement, Notification
@@ -7,6 +8,19 @@ from accounts.models import Account, Comment, Faculty, Announcement, Notificatio
 @admin.register(Permission)
 class PermissionAdmin(admin.ModelAdmin):
     exclude = []
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'user_full_name', 'get_edited_object', 'action_time')
+
+    def user_full_name(self, obj):
+        return obj.user.get_full_name()
+    user_full_name.short_description = "Пользователь"
+
+    def get_edited_object(self, obj):
+        return obj.get_edited_object()
+    get_edited_object.short_description = "Измененный объект"
 
 
 @admin.register(Faculty)
@@ -40,7 +54,7 @@ class AccountAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author_name', 'object_type', 'object', 'short_text', 'is_deleted', 'date_created')
+    list_display = ('id', 'author_full_name', 'object_type', 'object', 'short_text', 'is_deleted', 'date_created')
     list_editable = ('is_deleted',)
     list_select_related = ('author', 'object_type', 'object')
     search_fields = ('author__last_name', 'author__first_name')
@@ -52,7 +66,7 @@ class CommentAdmin(admin.ModelAdmin):
             'fields': (('object_type', 'object_id', 'object'),)
         }),
         ('Ссылки', {
-            'fields': ('author_name',)
+            'fields': ('author_full_name',)
         }),
         ('Детали', {
             'fields': ('text', 'is_deleted')
@@ -61,11 +75,11 @@ class CommentAdmin(admin.ModelAdmin):
             'fields': ('date_created',)
         })
     )
-    readonly_fields = ('date_created', 'object', 'author_name')
+    readonly_fields = ('date_created', 'object', 'author_full_name')
 
-    def author_name(self, obj):
+    def author_full_name(self, obj):
         return obj.author.get_full_name()
-    author_name.short_description = "Автор"
+    author_full_name.short_description = "Автор"
 
     def short_text(self, obj):
         return obj.text[:60] + (obj.text[60:] and "...")
