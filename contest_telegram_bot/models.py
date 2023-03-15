f4fca0a646259b90d1d75b3b19cae0f698f278f2
svelval <93996330+svelval@users.noreply.github.com>
2023-03-15 10:29:17 +0500

    Null contest_user now allowed

--- contest_telegram_bot/models.py
+++ contest_telegram_bot/models.py
@@ -3,18 +3,17 @@ from django.db import models
 
 
 class TelegramEntity(models.Model):
-    chat_id = models.PositiveBigIntegerField(verbose_name="ID телеграм-сущности")
+    chat_id = models.BigIntegerField(verbose_name="ID телеграм-сущности")
 
     class Meta:
         abstract = True
 
 
 class TelegramUser(TelegramEntity):
-    contest_user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь на Контесте")
+    contest_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name="Пользователь на Контесте")
 
     class Meta:
         verbose_name = "Пользователь в телеграме"
-
 
 # class TelegramGroup(TelegramEntity):
 #     contest_user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь на Контесте")
