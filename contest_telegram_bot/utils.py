83e042794abc197d20ae2f8166574212ffb4e75f
svelval <93996330+svelval@users.noreply.github.com>
2023-03-23 17:11:27 +0500

    - Table title added to courses, contests and problems tables - tg_authorisation_wrapper

--- contest_telegram_bot/utils.py
+++ contest_telegram_bot/utils.py
@@ -6,7 +6,8 @@ from emoji import emojize
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 
 from accounts.models import Account
-from contest_telegram_bot.constants import help_btn_text, login_btn_text, logout_btn_text
+from contest_telegram_bot.constants import help_btn_text, login_btn_text, logout_btn_text, courses_emoji, contest_emoji, \
+    bot_settings_emoji, user_settings_emoji
 
 from contest_telegram_bot.models import TelegramUser
 from contests.models import Assignment, Contest, Course
@@ -91,32 +92,39 @@ def staff_table_keyboard(contest_user: User, table_id: int = None):
     return keyboard
 
 
-# 1. Login_required decorator for bot; 2. save 3 functions for 3 keyboards; 3. Emojis for course list,
-#  for settings, etc.; 4. Bot settings and user settings; 5. Logout keyboard (inline or ordinary?)
+# TODO:
+#  3. Non_auth -> unauth VEZDE!!
+#  4. Bot settings and user settings;
+#  5. Logout keyboard (inline or ordinary?)
 
 def student_table_keyboard(table_type: str, contest_user: User, table_id: int = None):
     keyboard = InlineKeyboardMarkup(row_width=1)
     table_list = None
+    table_title = None
     table_header = None
     back_btn = None
     if table_type == 'courses':
         table_list = list([credit.course, credit.score] for credit in contest_user.credit_set.all())
+        table_title = [f'{courses_emoji} Ваши курсы']
         table_header = ['Курс', 'Оценка']
         back_btn = None
     elif table_type == 'contests':
         table_list = list(set(assignment.contest for assignment in
                               contest_user.assignment_set.filter(problem__contest__course_id=table_id)))
         table_list.reverse()
-        table_header = ['Раздел']
+        table_title = [f'{contest_emoji} {Course.objects.get(pk=table_id)}']
+        table_header = ['Разделы']
         back_btn = goback_button(goback_type='back', to='courses_list')
     elif table_type == 'problems':
         table_list = list([problem.problem, problem.score] for problem in
                           contest_user.assignment_set.filter(problem__contest_id=table_id))
         table_list.reverse()
+        table_title = [f'{contest_emoji} {Contest.objects.get(pk=table_id)}']
         table_header = ['Задача', 'Оценка']
         contest = Contest.objects.get(pk=table_id)
         back_btn = goback_button(goback_type='back', to='course', to_id=contest.course_id)
 
+    none_type_row(keyboard, table_title)
     none_type_row(keyboard, table_header)
     if len(table_list) == 0:
         none_type_row(keyboard, ['Заданий нет'])
@@ -131,53 +139,28 @@ def student_table_keyboard(table_type: str, contest_user: User, table_id: int = 
         for contest in table_list:
             keyboard.row(goback_button(goback_type='go', to='contest', to_id=contest.id, text=str(contest)))
 
+    if table_type == 'courses':
+        keyboard.row(InlineKeyboardButton(text=f'{user_settings_emoji} Настройки пользователя', callback_data=json.dumps({'type': 'setting'})))
+        keyboard.row(InlineKeyboardButton(text=f'{bot_settings_emoji} Настройки бота', callback_data=json.dumps({'type': 'setting'})))
+        keyboard.row(InlineKeyboardButton(text=logout_btn_text,
+                                          callback_data=json.dumps({'type': 'exit'})))
+
     if back_btn is not None:
         keyboard.row(back_btn)
     return keyboard
 
 
-
-
-
-# def courses_table_keyboard(contest_user: User):
-#     courses_keyboard = InlineKeyboardMarkup(row_width=1)
-#     courses_list = list([credit.course, credit.score] for credit in contest_user.credit_set.all())
-#     none_type_row(courses_keyboard, ['Курс', 'Оценка'])
-#     for course, course_score in courses_list:
-#         courses_keyboard.row(
-#             InlineKeyboardButton(text=str(course),
-#                                  callback_data=json.dumps({'type': 'go', 'to': 'course', 'id': course.id})),
-#             score_button(course_score)
-#         )
-#     return courses_keyboard
-#
-#
-# def contests_table_keyboard(contest_user: User, course_id: int):
-#     contests_keyboard = InlineKeyboardMarkup(row_width=1)
-#     contests_list = list(set(assignment.contest for assignment in
-#                              contest_user.assignment_set.filter(problem__contest__course_id=course_id)))
-#     none_type_row(contests_keyboard, ['Раздел'])
-#     for contest in reversed(contests_list):
-#         contests_keyboard.row(InlineKeyboardButton(text=str(contest),
-#                                                    callback_data=json.dumps(
-#                                                        {'type': 'go', 'to': 'contest', 'id': contest.id})))
-#     contests_keyboard.row(back_button('courses_list'))
-#     return contests_keyboard
-#
-#
-# def problems_table_keyboard(contest_user: User, contest_id: int):
-#     contest = Contest.objects.get(pk=contest_id)
-#     problems_keyboard = InlineKeyboardMarkup(row_width=1)
-#     problems_list = list([problem.problem, problem.score] for problem in
-#                          contest_user.assignment_set.filter(problem__contest_id=contest_id))
-#     none_type_row(problems_keyboard, ['Задача', 'Оценка'])
-#     for problem, problem_score in reversed(problems_list):
-#         problems_keyboard.row(InlineKeyboardButton(text=str(problem),
-#                                                    callback_data=json.dumps(
-#                                                        {'type': 'go', 'to': 'problem', 'id': problem.id})),
-#                               score_button(problem_score))
-#     problems_keyboard.row(back_button('course', contest.course_id))
-#     return problems_keyboard
+def tg_authorisation_wrapper(
+        chat_id: int,
+        authorized_fun,
+        unauthorized_fun,
+        auth_fun_args: dict,
+        unauth_fun_args: dict
+):
+    if get_telegram_user(chat_id=chat_id) is None:
+        unauthorized_fun(**auth_fun_args)
+    else:
+        authorized_fun(**unauth_fun_args)
 
 
 def json_get(json_str: str, key: str):
