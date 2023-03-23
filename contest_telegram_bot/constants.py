f0a2cef09a076e142fef97b0e7f25a880457c749
svelval <93996330+svelval@users.noreply.github.com>
2023-03-23 17:10:02 +0500

    courses, contest, bot_settings, user_settings emojis added

--- contest_telegram_bot/constants.py
+++ contest_telegram_bot/constants.py
@@ -1,5 +1,9 @@
 from emoji import emojize
 
 login_btn_text = emojize(":locked_with_key: Войти")
-logout_btn_text = emojize(":left_arrow: Выйти")
+logout_btn_text = emojize(":left_arrow: Выйти из системы")
 help_btn_text = emojize(":books: Помощь")
+courses_emoji = emojize(":books:")
+contest_emoji = emojize(":open_book:")
+bot_settings_emoji = emojize(":hammer_and_wrench:")
+user_settings_emoji = emojize(":gear:")
