23b643d6ca096abbcbee544b0b9e71408c053301
svelval <93996330+svelval@users.noreply.github.com>
2023-05-01 19:49:31 +0500

    staff_start_keyboard() -> staff_and_moders_start_keyboard()

--- contest_telegram_bot/keyboards.py
+++ contest_telegram_bot/keyboards.py
@@ -101,7 +101,7 @@ def settings_keyboard(contest_user: User):
     return keyboard
 
 
-def staff_start_keyboard(staff_contest_user: User):
+def staff_and_moders_start_keyboard(staff_contest_user: User, for_moders=False):
     keyboard = InlineKeyboardMarkup(row_width=1)
     table_header = [f'{courses_emoji} Ваши курсы']
     table_list = Course.objects.filter(leaders__account=Account.objects.get(user=staff_contest_user))
