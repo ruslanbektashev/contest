166977b6f1244bee2b53fca92e2c8fab49f4806f
svelval <93996330+svelval@users.noreply.github.com>
2023-05-08 17:54:36 +0500

    TelegramUserSettings fields verbose_names changed

--- contest_telegram_bot/models.py
+++ contest_telegram_bot/models.py
@@ -18,16 +18,16 @@ class TelegramUser(TelegramEntity):
 
 class TelegramUserSettings(models.Model):
     contest_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name="Пользователь на Контесте")
-    comments = models.BooleanField(verbose_name='Оповещения о комментариях', default=True)
-    announcements = models.BooleanField(verbose_name='Оповещения об объявлениях', default=True)
-    schedules = models.BooleanField(verbose_name='Оповещения о расписании', default=False)
-    submissions = models.BooleanField(verbose_name='Оповещения об отправляемых посылках', default=False)
-    submissions_mark = models.BooleanField(verbose_name='Оповещения об оценке за посылки', default=True)
-    assignments_mark = models.BooleanField(verbose_name='Оповещения об оценке за задачи', default=True)
-    courses_mark = models.BooleanField(verbose_name='Оповещения об оценке за курс', default=True)
-    questions = models.BooleanField(verbose_name='Оповещения о вопросах', default=True)
-    reports = models.BooleanField(verbose_name='Оповещения о сообщениях об ошибках', default=True)
-    assignments = models.BooleanField(verbose_name='Оповещения о назначениях задач', default=True)
+    comments = models.BooleanField(verbose_name='Комментарии', default=True)
+    announcements = models.BooleanField(verbose_name='Объявления', default=True)
+    schedules = models.BooleanField(verbose_name='Расписание', default=False)
+    submissions = models.BooleanField(verbose_name='Отправленные посылки', default=False)
+    submissions_mark = models.BooleanField(verbose_name='Оценки за посылки', default=True)
+    assignments_mark = models.BooleanField(verbose_name='Оценки за задачи', default=True)
+    courses_mark = models.BooleanField(verbose_name='Оценки за курсы', default=True)
+    questions = models.BooleanField(verbose_name='Вопросы', default=True)
+    reports = models.BooleanField(verbose_name='Сообщения об ошибках', default=True)
+    assignments = models.BooleanField(verbose_name='Назначение задач', default=True)
 
     class Meta:
         verbose_name = "Настройки телеграм-пользователя"
