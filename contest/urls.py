87f328a55c5c41ef962611b2c90109f6f31c9fc5
Ruslan Bektashev <ruslanbektashev@gmail.com>
2024-03-31 21:50:35 +0300

    fix protected upload once again

--- contest/urls.py
+++ contest/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from contest import views
 
@@ -42,6 +42,6 @@ urlpatterns = [
     path('schedule/', include('schedule.urls')),
     path('support/', include('support.urls')),
     path('admin/', admin.site.urls),
-    path('upload/', views.ProtectedUpload.as_view(), name='protected-upload')
+    re_path(r'^upload/', views.ProtectedUpload.as_view(), name='protected-upload')
 ] + (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
      static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
--- contest/views.py
+++ contest/views.py
@@ -1,11 +1,20 @@
+import mimetypes
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.utils.encoding import iri_to_uri
 from django.views import View
 
 
 class ProtectedUpload(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        response = HttpResponse(status=200)
-        response['Content-Type'] = ''
-        response['X-Accel-Redirect'] = request.path
+        content_type, encoding = mimetypes.guess_type(request.path)
+        content_type = content_type or 'application/octet-stream'
+        response = HttpResponse(content_type=content_type)
+        if encoding:
+            response['Content-Encoding'] = encoding
+        try:
+            response['X-Accel-Redirect'] = '/protected/' + iri_to_uri(request.path)
+        except UnicodeEncodeError:
+            raise Http404("Страница не найдена")
         return response
